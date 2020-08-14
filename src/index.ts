1ecf1f5e8f1414e5451012f44a16bf34bd40868c
jeffreza <jeffreza041992@gmail.com>
2020-08-15 00:52:34 +0500

    clientService

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import http from 'http';
+import axios from 'axios';
+import service from './server/server';
+import Config from './config/index';
+
+const config = new Config('debug');
+const server: any = http.createServer(service);
+
+async function initialize() {
+  server.listen(0);
+  server.on('listening', () => {
+    const registerService = () =>
+      axios.put(
+        `http://localhost:3000/register/${config.name}/${config.version}/${
+          server.address().port
+        }`
+      );
+    registerService();
+    const interval = setInterval(registerService, 20000);
+    config.log.info(
+      `Hi there! I'm listening on port ${
+        server.address().port
+      } in ${service.get('env')} mode.`
+    );
+  });
+}
+
+initialize();
